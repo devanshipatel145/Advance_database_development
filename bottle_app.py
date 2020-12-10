48ef7475974f68a8d3d8b5662aed84174ba630af
devanshipatel145 <62411278+devanshipatel145@users.noreply.github.com>
2020-12-09 22:40:31 -0500

    dec 7 updates

--- bottle_app.py
+++ bottle_app.py
@@ -1,69 +1,75 @@
 # A very simple Bottle Hello World app for you to get started with...
+import datetime
+import time
 import os
-from bottle import get, post, template, request, redirect
+import pymongo
+from bson.objectid import ObjectId
 
-# are we executing at PythonAnywhere?
-ON_PYTHONANYWHERE = "PYTHONANYWHERE_DOMAIN" in os.environ
 
-# assert ON_PYTHONANYWHERE == True
+client = pymongo.MongoClient("mongodb+srv://alpha-user:alpha-1234@soliton.zk5ax.mongodb.net/todo?retryWrites=true&w=majority",
+                             connectTimeoutMS=30000,
+                             socketTimeoutMS=None,
+                             # socketKeepAlive=True,
+                             connect=False, maxPoolsize=1)
 
-if ON_PYTHONANYWHERE:
-    # on PA, set up to connect to the WSGI server
-    from bottle import default_app
-else:
-    # on the development environment, import the development server
-    from bottle import run, debug
+db = client.todo
 
-from postgres_storage import get_items, get_item, update_status, create_item, update_item, delete_item
+from bottle import get, post, request, response, template, redirect, default_app
 
+# APPLICATION PAGES AND ROUTES
+
+# assume collection contains fields "id", "task", "status"
 
 @get('/')
 def get_show_list():
-    result = get_items()
-    return template("show_list", rows=result)
+    result = db.task.find()
+    # return list(result)
+    # get all the items in the collection as a list of dictionaries
+    return template("show_list", rows=result, session={})
 
 
-@get("/set_status/<id:int>/<value:int>")
-def get_set_status(id, value):
-    update_status(id, value)
-    redirect("/")
+@get('/update_status/<_id>/<value:int>')
+def get_update_status(_id, value):
+    result = db.task.update_one( {"_id" : ObjectId(_id)}, {'$set': {'status': (value!=0)}} )
+    redirect('/')
 
 
-@get("/new_item")
+@get('/delete_item/<_id>')
+def get_delete_item(_id):
+    # given an id, delete the relevant document
+    result = db.task.delete_one( {"_id": ObjectId(_id)} )
+    redirect('/')
+
+
+@get('/update_task/<_id>')
+def get_update_task(_id):
+    # given an id, get the document and populate a form
+    result = db.task.find_one( {"_id": ObjectId(_id)} )
+    return template("update_task", row=dict(result))
+
+@post('/update_task')
+def post_update_task():
+    # given an id and an updated task in a form, find the document and modify the task value
+    _id = request.forms.get("_id").strip()
+    updated_task = request.forms.get("updated_task").strip()
+    result = db.task.update_one( {"_id" : ObjectId(_id)}, {'$set': {'task': updated_task}} )
+    redirect('/')
+
+@get('/new_item')
 def get_new_item():
     return template("new_item")
 
 
-@post("/new_item")
+@post('/new_item')
 def post_new_item():
-    new_item = request.forms.get("new_item").strip()
-    create_item(new_item, 1)
-    redirect("/")
+    # given a new task in a form, create and insert a document and with that task value
+    new_task = request.forms.get("new_task").strip()
+    result = db.task.insert_one( {'task': new_task, 'status':False} )
+    redirect('/')
 
-@get("/update_item/<id:int>")
-def get_update_item(id):
-    result = get_item(id)
-    return template("update_item", row=result)
+application = default_app()
 
-
-@post("/update_item")
-def post_update_item():
-    id = int(request.forms.get("id").strip())
-    updated_item = request.forms.get("updated_item").strip()
-    update_item(id, updated_item)
-    redirect("/")
-
-
-@get("/delete_item/<id:int>")
-def get_delete_item(id):
-    delete_item(id)
-    redirect("/")
-
-if ON_PYTHONANYWHERE:
-    # on PA, connect to the WSGI server
-    application = default_app()
-else:
-    # on the development environment, run the development server
-    debug(True)
-    run(host='localhost', port=8080)
-
+if __name__ == "__main__":
+    #db.tasks.insert_one({'id':1, 'task':"read a book on Mongo", "status":False})
+    #db.tasks.insert_one({'id':2, 'task':"read a another book on PyMongo", "status":False})
+    print(get_show_list())
--- show_list.tpl
+++ show_list.tpl
@@ -1,22 +1,33 @@
-<p>Todo List - Experimental Version</p>
-<table border="1">
+<html>
+<head>
+<title>Todo List 0.001</title>
+<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet"/>
+<link href="https://www.w3schools.com/w3css/4/w3.css" rel="stylesheet" >
+</head>
+<body>
+%include("header.tpl", session=session)
+<table class="w3-table w3-bordered w3-border">
 %for row in rows:
     <tr>
-        <td>{{row[0]}}</td>
         <td>
-            <a href="/update_item/{{row[0]}}">{{row[1]}}</a>
+            <a href="/update_task/{{row['_id']}}"><i class="material-icons">edit</i></a>
         </td>
         <td>
-        %if row[2]==0:
-            <a href="/set_status/{{row[0]}}/1">{{row[2]}}</a>
-        %else:
-            <a href="/set_status/{{row[0]}}/0">{{row[2]}}</a>
-        %end
+            {{row['task']}}
+        </td>
         <td>
-            <a href="/delete_item/{{row[0]}}">DELETE</a>
+        %if row['status']==0:
+            <a href="/update_status/{{row['_id']}}/1"><i class="material-icons">check_box_outline_blank</i></a>
+        %else:
+            <a href="/update_status/{{row['_id']}}/0"><i class="material-icons">check_box</i></a>
+        %end
+        </td>
+        <td>
+            <a href="/delete_item/{{row['_id']}}"><i class="material-icons">delete</i></a>
         </td>
     </tr>
 %end
 </table>
-<a href="/new_item">New Item...</a>
-<hr/>
+%include("footer.tpl", session=session)
+</body>
+</html>
